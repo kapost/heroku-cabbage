0a15bbf859d6441481acfb58cc69c97c90bb80ac
Brandon Croft <brandon.croft@gmail.com>
2016-03-22 11:20:39 -0600

    'slack' option is now 'hook' option

--- lib/kapost-provision/commands/cabbage.rb
+++ lib/kapost-provision/commands/cabbage.rb
@@ -15,7 +15,7 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
   #
   # Provisions a new app for all Kapost deploy environments
   #
-  # -s, --slack TOKEN # The slack channel token for deployhook config
+  # -h, --hook URL # optional deploy hook URL
   #
   def provision
     base_name = args.shift
@@ -24,7 +24,7 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
       error('Do you need help finding a cabbage name? Here are some suggestions: https://en.wikipedia.org/wiki/Cruciferous_vegetables#List_of_cruciferous_vegetables')
     end
 
-    slack_token = options[:slack]
+    http_hook = options[:hook]
 
     message = []
     message << 'This action will:'
@@ -32,7 +32,7 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
     message << '1. Install heroku-pipelines addon'
     message << "2. Create the Heroku apps #{app_names_to_create(base_name)}"
     message << "3. Add email deploy hook for #{DEPLOY_EMAIL_RECIPIENTS}"
-    message << "4. Add deploy hook to Slack webhook #{slack_webhook_url(slack_token)}" if slack_token
+    message << "4. Add http deploy hook #{http_hook}" if http_hook
     message << ''
     message << 'Are you sure you want to continue? (y/n)'
     return unless confirm(message.join("\n"))
@@ -42,7 +42,7 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
     KAPOST_ENVS.each.with_index do |env, index|
       heroku_name = "#{base_name}-#{env[:name]}c"
 
-      create_heroku_app(heroku_name, slack_token)
+      create_heroku_app(heroku_name, http_hook)
       add_app_to_pipeline(app_name, heroku_name, env[:pipeline_stage], index == 0)
     end
   end
@@ -65,20 +65,16 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
     end
   end
 
-  def create_heroku_hooks(heroku_name, heroku_slack_token)
+  def create_heroku_hooks(heroku_name, http_hook)
     system("heroku addons:create deployhooks:email --app #{heroku_name} --recipient=\"#{DEPLOY_EMAIL_RECIPIENTS}\" --subject=\"Deployed $1 to $i\" --body=\"{{git_log}}\"")
 
-    if heroku_slack_token
-      system("heroku addons:create deployhooks:http --app #{heroku_name} --url=#{slack_webhook_url(heroku_slack_token)}")
+    if http_hook
+      system("heroku addons:create deployhooks:http --app #{heroku_name} --url=#{http_hook}")
     end
   end
 
-  def slack_webhook_url(token)
-    "https://kapost.slack.com/services/hooks/heroku?token=#{token}"
-  end
-
-  def create_heroku_app(heroku_name, heroku_slack_token)
+  def create_heroku_app(heroku_name, http_hook)
     system("heroku apps:create #{heroku_name} -o kapost")
-    create_heroku_hooks(heroku_name, heroku_slack_token)
+    create_heroku_hooks(heroku_name, http_hook)
   end
 end
