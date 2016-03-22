76ae5e8c0329586f278d64307329401bd7867a4c
Brandon Croft <brandon.croft@gmail.com>
2016-03-22 13:03:55 -0600

    Don't install heroku pipelines plugin unless necessary

--- lib/kapost-provision/commands/cabbage.rb
+++ lib/kapost-provision/commands/cabbage.rb
@@ -9,7 +9,7 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
     { name: 'prod',     pipeline_stage: 'production' }
   ].freeze
 
-  DEPLOY_EMAIL_RECIPIENTS = 'pe@kapost.com deploynotifications@kapost.com'
+  DEPLOY_EMAIL_RECIPIENTS = 'pe@kapost.com deploynotifications@kapost.com'.freeze
 
   # cabbage:provision APP_NAME
   #
@@ -21,18 +21,19 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
     base_name = args.shift
 
     unless base_name
-      error('Do you need help finding a cabbage name? Here are some suggestions: https://en.wikipedia.org/wiki/Cruciferous_vegetables#List_of_cruciferous_vegetables')
+      error('Do you need help finding a cabbage name? https://en.wikipedia.org/wiki/Cruciferous_vegetables#List_of_cruciferous_vegetables')
     end
 
     http_hook = options[:hook]
 
     message = []
+    n = 0
     message << 'This action will:'
     message << ''
-    message << '1. Install heroku-pipelines addon'
-    message << "2. Create the Heroku apps #{app_names_to_create(base_name)}"
-    message << "3. Add email deploy hook for #{DEPLOY_EMAIL_RECIPIENTS}"
-    message << "4. Add http deploy hook #{http_hook}" if http_hook
+    message << "#{n += 1}. Install heroku-pipelines addon" unless pipelines_installed?
+    message << "#{n += 1}. Create the Heroku apps #{app_names_to_create(base_name)} in pipeline #{base_name}"
+    message << "#{n += 1}. Add email deploy hook for #{DEPLOY_EMAIL_RECIPIENTS}"
+    message << "#{n += 1}. Add http deploy hook #{http_hook}" if http_hook
     message << ''
     message << 'Are you sure you want to continue? (y/n)'
     return unless confirm(message.join("\n"))
@@ -53,24 +54,22 @@ class Heroku::Command::Cabbage < Heroku::Command::Base
     KAPOST_ENVS.map { |env| "#{app_name}-#{env[:name]}c" }.join(' ')
   end
 
+  def pipelines_installed?
+    `heroku plugins` =~ /^heroku-pipelines@/
+  end
+
   def install_pipelines
-    system('heroku plugins:install heroku-pipelines')
+    system('heroku plugins:install heroku-pipelines') unless pipelines_installed?
   end
 
   def add_app_to_pipeline(pipeline_name, heroku_app, stage, is_first_run)
-    if is_first_run
-      system("heroku pipelines:create #{pipeline_name} -a #{heroku_app} --stage #{stage}")
-    else
-      system("heroku pipelines:add #{pipeline_name} -a #{heroku_app} --stage #{stage}")
-    end
+    add_or_create = is_first_run ? 'create' : 'add'
+    system("heroku pipelines:#{add_or_create} #{pipeline_name} -a #{heroku_app} --stage #{stage}")
   end
 
   def create_heroku_hooks(heroku_name, http_hook)
     system("heroku addons:create deployhooks:email --app #{heroku_name} --recipient=\"#{DEPLOY_EMAIL_RECIPIENTS}\" --subject=\"Deployed $1 to $i\" --body=\"{{git_log}}\"")
-
-    if http_hook
-      system("heroku addons:create deployhooks:http --app #{heroku_name} --url=#{http_hook}")
-    end
+    system("heroku addons:create deployhooks:http --app #{heroku_name} --url=#{http_hook}") if http_hook
   end
 
   def create_heroku_app(heroku_name, http_hook)
